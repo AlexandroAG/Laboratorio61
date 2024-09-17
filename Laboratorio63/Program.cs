3d18e43d9967d0fc8f03e84abc9377acd2b9e90c
Arturo Rodriguez <arturo.rodriguez@utp.ac.pa>
2024-09-16 19:47:10 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio63/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Laboratorio63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                int[] MyNumbers = { 1, 2, 3 };
+                Console.WriteLine(MyNumbers[10]);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Algo salio mal, valide el indice del Arreglo");
+
+            }
+            finally
+            {
+
+                Console.WriteLine("Continuacion de la aplicacion, luego del bloque try/catch");
+
+            }
+
+        }
+    }
+}
---
+++ Laboratorio64/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laboratorio64
+{
+    class Program
+    {
+        static void CheckAge(int age)
+        {
+
+             if (age< 10 )
+             {
+                throw new ArithmeticException("Acceso negado - No cumple con el criterio de Edad");                                           
+             }
+             else 
+             {
+                Console.WriteLine("Accseso Concedido");
+             }
+        }
+
+        static void Main(String[] args)
+        {
+            CheckAge(15);
+        }
+    
+    } 
+}
